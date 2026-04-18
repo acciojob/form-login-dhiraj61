bf56c20583e2ad39d5dcca267f0b64c8c1d61947
Dhiraj Chaudhari <141726259+dhiraj61@users.noreply.github.com>
2026-04-18 09:25:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
-
+	let fname = document.getElementsByName("fname").value;
+	let lname = document.getElementsByName("lname").value;
+	alert(`${fname} ${lname}`);
 }
+
+document.getElementById("form1").addEventListener("submit",(event)=>{
+	event.preventDefault();
+	getFormvalue();
+})
